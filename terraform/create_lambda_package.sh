828fb033736a50a932792b08efb651833767333a
John Barber <jsbarber@gmail.com>
2019-07-13 13:33:43 +0100

    update lambda function package script with hash matching to only run on change

--- terraform/create_lambda_package.sh
+++ terraform/create_lambda_package.sh
@@ -13,6 +13,20 @@ if [ ! -d "lambda_functions/$function" ]; then
     exit 1
 fi
 
+echo "Checking for function updates via hash file"
+shafile="./.$function.sha"
+newhash=$(find lambda_functions/$function -type f -print0 | sort -z | xargs -0 sha1sum | sha1sum| sed 's/ //g')
+echo "newhash = $newhash"
+# If hash file exists - compare. If not continue as need to generate and run regardless
+if [ -f ./.$function.sha ]; then
+    oldhash=$(cat $shafile | sed 's/ //g')
+    echo "oldhash = $oldhash"
+    if [ "$oldhash" == "$newhash" ]; then
+        echo "Hashes match - no change detected. Exiting"
+        exit 0
+    fi
+fi
+
 pushd ./lambda_functions/$function
 zipfile="function.zip"
 if [ -f $zipfile ]; then
@@ -22,7 +36,9 @@ fi
 
 echo "Creating new function.zip file"
 
-zip -r $zipfile ./*
+zip -r -q $zipfile ./*
+
+
 if [ $? -eq 0 ]; then
     rm -f $zipfile.old
 else
@@ -32,6 +48,9 @@ else
     exit 1
 fi
 popd
+# Update hash file
+newhash=$(find lambda_functions/$function -type f -print0 | sort -z | xargs -0 sha1sum | sha1sum)
+echo $newhash > $shafile
 
 echo "Success - $zipfile created. Now run terraform to update infra with new function code"
 exit 0
